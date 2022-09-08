308e47113d16608c9e8ffef2bf5adcf4c3ff651a
Lucas Peluso <lucaspelusoweb@gmail.com>
2022-09-08 16:53:02 -0300

    delete comment and console.log

--- fetchDataSproduct.js
+++ fetchDataSproduct.js
@@ -1,18 +1,11 @@
 const productDetails = document.querySelector('#product-details')
-console.log(productDetails)
 const productDetailsFather = document.querySelector('#product-details__father')
-console.log(productDetailsFather)
 
-const fragment = document.createDocumentFragment();
 
 const getDetailsProduct = data => {
-    console.log('Hola data')
-    console.log(data)
     let id = localStorage.getItem("id")
-    console.log(id + " hola perro")
     data.forEach(product => {
         if (product.id == id) {
-            console.log(id + " joyañom")
             productDetails.querySelector('h5').textContent = product.title;
             productDetails.querySelector('p').textContent = `$${product.price}.00`;
             productDetails.querySelector('img').setAttribute("src", product.img);
